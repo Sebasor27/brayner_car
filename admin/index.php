1a3b53acc07a914ffbacc56f8807c964cec41ad7
Sebasor27za <sebasicha27@gmail.com>
2024-07-03 18:36:51 -0500

    commit e irme a casa

--- admin/index.php
+++ admin/index.php
@@ -1,3 +1,10 @@
+<?php
+ require("../admin/inc/essentials.php");
+ require("../admin/inc/db_config.php");
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -18,14 +25,14 @@
 <body class="bg-ligth">
     
  <div class="login-form text-center rounded bg-white shadow overflow-none">
-    <form>
+    <form method="POST">
         <h4 class="bg-dark text-white py-3">ADMIN LOGIN PANEL</h4>
         <div class="p-4">
                         <div class="mb-3">
-                            <input name="admin_name" type="text" class="form-control shadow-none text-center" placeholder="Admin Name">
+                            <input name="admin_name" required type="text" class="form-control shadow-none text-center" placeholder="Admin Name">
                         </div>
                         <div class="mb-3">
-                            <input name="admin_pass" type="password" class="form-control shadow-none" placeholder="Password">
+                            <input name="admin_pass" required type="password" class="form-control shadow-none" placeholder="Password">
                         </div>
                         <div>
                             <button name="login" type="submit" class="btn custom-bg shadows-none">Entrar</button>
@@ -36,7 +43,22 @@
 
 
 
-
+   <?php
+           if(isset($_POST['login']))
+           {
+             $frm_data = filteration($_POST);
+             $query = "SELECT * FROM `admin_cred` WHERE `admin_name`=? AND `admin_pass`=?";
+             $values = [$frm_data['admin_name'],$frm_data['admin_pass']];
+             
+             $res = select($query,$values,"ss");
+             if($res->num_rows==1){
+                echo"tengo usuario";
+             }else{
+                
+             }
+           }
+           
+   ?>
 
 
 

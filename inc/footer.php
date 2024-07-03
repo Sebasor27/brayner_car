e97c2c027b0c6da560cc2faa8f61a219682581a6
Sebasor27za <sebasicha27@gmail.com>
2024-07-03 11:23:04 -0500

    parte2

---
+++ inc/footer.php
@@ -0,0 +1,32 @@
+<div class="container-fluid bg-white mt-5">
+    <div class="row">
+        <div class="col-lg-4 p-4">
+           <h3 class="h-font fw-bold fs-3 mb-2">Brayner Car</h3>
+           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident inventore, fugiat tempora corporis sapiente vel facilis ducimus officia quasi, 
+            voluptatum atque dolor 
+            iure unde qui doloribus delectus neque accusantium mollitia?</p>
+        </div>
+        <div class="col-lg-4 p-4">
+           <h5 class="mb-3">Links</h5>
+           <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Pagina Principal</a><br>
+           <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Carros</a><br>
+           <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Servicios</a><br>
+           <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Conatctenos</a><br>
+           <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Acerca de nosotros</a>
+
+        </div>
+        <div class="col-lg-4 p-4">
+            <h5 class="mb-3">Siguenos en Facebook</h5>
+            <a href=""class="d-inline-block mb-3">
+                 <span class="badge bg-ligth text-dark fs-6 p-2">
+                 <i class="bi bi-facebook me-1"></i> Red Social
+                 </span>
+                </a>
+                <br>
+                <a href="tel: +593 989645329" class="d-inline-block text-decration-none text-dark mb-2">
+                <i class="bi bi-car-front"></i> Brayner_Car
+            </a>
+        </div>
+
+    </div>
+   </div>
